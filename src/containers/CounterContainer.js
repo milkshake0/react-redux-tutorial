52b38f6b4661c1459dbd9e9f443dc27c35739777
milkshake0 <jiwoo992020@gmail.com>
2021-04-23 21:41:01 +0900

    useSelector, useDispatch Hooks

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,32 +1,18 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter
+      number={number}
+      onIncrease={useCallback(() => dispatch(increase()), [dispatch])}
+      onDecrease={useCallback(() => dispatch(decrease()), [dispatch])}
+    />
   );
 };
 
-// const mapStateToProps = (state) => {
-//   console.log(state);
-//   return {
-//     number: state.counter.number,
-//   };
-// };
-
-// const mapDispatchToProps = (dispatch) =>
-//   bindActionCreators({ increase, decrease }, dispatch);
-
-// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
-
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  }
-)(CounterContainer);
+export default CounterContainer;
---
+++ src/lib/useActions.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const useActions = () => {
+  return <div></div>;
+};
+
+export default useActions;
